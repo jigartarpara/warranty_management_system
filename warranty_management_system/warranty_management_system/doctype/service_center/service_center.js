0b5dfd2db062c38be2f288fdf0dfba3daf005d22
Jigar Tarpara <jigartarpara68@gmail.com>
2020-08-08 11:48:54 +0530

    Serive Center

---
+++ warranty_management_system/warranty_management_system/doctype/service_center/service_center.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Jigar Tarpara and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Center', {
+	// refresh: function(frm) {
+
+	// }
+});
